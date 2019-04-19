da7134ef0b74e96bd97727151a574cb5d20e7530
krashaen <krashaen@gmail.com>
2019-04-19 18:35:15 +0700

    fix regexp

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,8 +14,8 @@ $(window).resize(sectionHeight);
 
 $(function() {
   $("section h1, section h2, section h3").each(function(){
-    $("nav ul").append("<li class='list-item tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\[а-яa-zЁё]-]+/g,'') + "'>" + $(this).text() + "</a></li>");
-    $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\[а-яa-zЁё]-]+/g,''));
+    $("nav ul").append("<li class='list-item tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^[a-zа-яё\s]+$/g,'') + "'>" + $(this).text() + "</a></li>");
+    $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^[a-zа-яё\s]+$/g,''));
     $("nav ul li:first-child a").parent().addClass("active");
   });
 
